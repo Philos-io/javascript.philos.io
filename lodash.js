9d45853d3228e5fff06f8b69b087c522b69cec5f
davy engone <davy@philos.io>
2015-09-08 22:58:31 +0200

    added few spec

---
+++ lodash.js
@@ -0,0 +1,19 @@
+describe('Identity', function() {
+	
+});
+
+describe('head', function() {
+	
+});
+
+describe('tail', function() {
+	
+});
+
+describe('Object', function() {
+	
+});
+
+describe('Array', function() {
+	
+});
