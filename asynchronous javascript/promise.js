227575f43a33444c9a44766b124189a1395a1913
davy engone <davy@philos.io>
2015-09-16 05:56:44 +0200

    added more examples for native promise api

---
+++ asynchronous javascript/promise.js
@@ -0,0 +1,37 @@
+// JavaScript Native Promise API
+function getUsers(){
+    return new Promise(function(resolve, reject){
+        var users = [];
+
+        for (var i = 0; i < 100; i++) {
+          users.push({name: 'user'+i});
+        }
+        if (users.length > 0) {
+          resolve(users);
+        }else{
+          reject(users);
+        }
+    });
+
+}
+// now you have access to an API (then, done, catch, success, error, always)
+promise.then(success, error);
+
+function getData(){
+	var defer = Promise.defer();
+
+	setTimeout(function(){
+		defer.resolve(["davyengone"]);
+	});
+
+	return defer.promise;
+}
+
+
+var result = getData();
+
+console.log(result);
+
+result.then(function(data){
+	console.log(data)
+});
