2df820eb45e1fe03d311d306848a5f2a0e0260ac
davy engone <davy@philos.io>
2015-09-16 05:57:16 +0200

    added more examples for angular

--- asynchronous javascript/deferred-angular.js
+++ asynchronous javascript/deferred-angular.js
@@ -27,3 +27,89 @@ function getUserInformation($q, $http){
       var userRepo = results[1];
     });
 }
+
+
+angular.module('promise', [])
+
+.controller('MainCtrl', function($http){
+
+	$http.get('https://api.github.com/uses/davyengone')
+
+	.success(function(res){
+			console.log(res);
+			try{
+				throw new Error('something happens again');
+			}catch(e){
+				console.log(e)
+			}
+		})
+
+	.error(function(err){
+		console.log(err);
+	})
+	.catch(function(err){
+		console.log('okay, I got it', err);
+	});
+
+});
+
+
+angular.module('promise', [])
+
+.controller('MainCtrl', function($http){
+
+	$http.get('https://api.github.com/users/davyengone')
+		.then(function(res){
+			console.log('first pipe');
+			return res;
+		}).then(function(user){
+			console.log('second pipe', user.data);
+		 return user.data;
+		})
+	.catch(function(err){
+		console.log('okay, I got it', err);
+	});
+
+});
+
+angular.module('promise', [])
+
+.controller('MainCtrl', function($http, $q){
+
+	var userInfoPromise = $http.get('https://api.github.com/users/davyengone');
+	var reposPromise = $http.get('https://api.github.com/users/davyengone/repos');
+	var starredPromises = $http.get('https://api.github.com/users/davyengone/starred');
+
+
+	function getUserInformation(){
+		$q.all([userInfoPromise, reposPromise, starredPromises])
+			.then(function(res){
+				var user = res[0].data, repos = res[1].data, starred = res[2].data;
+				console.log(user);
+		});
+	}
+
+	getUserInformation();
+
+});
+
+angular.module('promise', [])
+
+.controller('MainCtrl', function($http, $q){
+
+	var userInfoPromise = $http.get('https://api.github.com/users/davyengone');
+	var reposPromise = $http.get('https://api.github.com/users/davyengone/repos');
+	var starredPromises = $http.get('https://api.github.com/users/davyengone/starred');
+
+
+	function getUserInformation(){
+		$q.all([userInfoPromise, reposPromise, starredPromises])
+			.then(function(res){
+				var user = res[0].data, repos = res[1].data, starred = res[2].data;
+				console.log(user);
+		});
+	}
+
+	getUserInformation();
+
+});
