9cdf16ab39ff9dbac170418537ee512f8cdbfb68
davy engone <davy@philos.io>
2015-09-16 05:58:16 +0200

    added events and resources examples

--- asynchronous javascript/synchronous-code.js
+++ asynchronous javascript/synchronous-code.js
@@ -1,9 +1,11 @@
 function getUsers(){
   var users = [];
-  for (var i = 0; i < 100000; i++) {
+  for (var i = 0; i < 1000; i++) {
     users.push({name: 'user'+i});
   }
   return users;
 }
 
 var users = getUsers();
+// What will be the output of this call?
+console.log(users);
